7466d43e18b24065c6595e6ee7910f05bb124dd5
Oberoner21 <t.schindel@gmx.de>
2026-09-23 19:35:14 +0200

    Test Rule JZ

--- rule_jc.h
+++ rule_jc.h
@@ -41,7 +41,7 @@ void GenerateRuleJC(uint16_t rule) {
 
     // Skip marked IR
     rule = next_RIGHT(rule);
-    SetRule(rule-1, '_', '_', RIGHT, rule+2);     // <-- Jump to END !!!!
+    SetRule(rule-1, '_', '_', RIGHT, rule+82);     // <-- Jump to END !!!!
 
     // ++++++++ Carry is set +++++++++++++
 
@@ -51,15 +51,76 @@ void GenerateRuleJC(uint16_t rule) {
     rule = next$RIGHT(rule);
     SetRule(rule-1, '$', '_', RIGHT, rule);
     // Seek to bit7 of MMB
-    rule = next_RIGHT(rule);
+    rule = next_RIGHT(rule);  // 86
 
-    // Copy low nibbel to interleaved PC
+    // Copy low nibbel of MMB to interleaved PC
+    for(i=0; i<4; i++){
+
+        // Skip to bit 3 of MMB
+        rule = nextRIGHT(rule);
+        rule = nextRIGHT(rule);
+        rule = nextRIGHT(rule);
+        rule = nextRIGHT(rule);
+
+        for(j=0; j<i; j++){
+
+            // Skip next left MMB bit
+            rule = nextRIGHT(rule);       
+        }
+
+        // Read current bit in MMB and branch
+        SetRule(rule, '0', '0', RIGHT, rule+4+i*2);
+        SetRule(rule, '1', '1', RIGHT, rule+1);
+        rule++;
+
+        // Readed bit was a 1
+        // Seek to P3 of PC
+        rule = next_LEFT(rule, LEFT); 
+        rule = next_LEFT(rule);
+
+        for(j=0; j<i; j++){
+
+            // Skip jx bit in PC
+            rule = nextRIGHT(rule);  
+            rule = nextRIGHT(rule);
+        }
+
+        // Write a 1 into current bit of PC
+        SetRule(rule, '0', '1', RIGHT, rule+4+i*2);
+        SetRule(rule, '1', '1', RIGHT, rule+4+i*2);  
+        rule++;
+
+        // Readed bit was a 0
+        // Seek to P3 of PC
+        rule = next_LEFT(rule, LEFT); 
+        rule = next_LEFT(rule);
+
+        for(j=0; j<i; j++){
+
+            // Skip jx bit in PC
+            rule = nextRIGHT(rule);  
+            rule = nextRIGHT(rule);
+        }
+
+        // Write a 0 into current bit of PC
+        SetRule(rule, '0', '0', RIGHT, rule+1);
+        SetRule(rule, '1', '0', RIGHT, rule+1);  
+        rule++;
+
+        // Jump back to bit7 of MMB
+        rule = next_RIGHT(rule);
+    }
+
+    // Change the last rule
+    SetRule(rule-1, '_', '_', LEFT, rule);
+    // Remark PC
+    rule = next_LEFT(rule);
+    SetRule(rule-1, '_', '$', RIGHT, rule);
     
-
     // ++++++++++++ END ++++++++++++++++++
 
     // Seek back to MMB an remark it
-    rule = next_RIGHT(rule);
+    rule = next_RIGHT(rule); 
     SetRule(rule-1, '_', '$', LEFT, rule);
     // Seek to marked register IR and remark it
     rule = next_LEFT(rule);
@@ -71,4 +132,6 @@ void GenerateRuleJC(uint16_t rule) {
     // Skip right Memory Address Register MAR and go to RULE_FETCH
     rule = next$RIGHT(rule);
     SetRule(rule-1, '$', '$', LEFT, RULE_FETCH);  
+
+    // 92 Rules
 }
